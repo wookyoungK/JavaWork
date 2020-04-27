fdb3d99ff53d79aa29d3fa0fdc08bfaed44edfdd
p11256 <pd1125@naver.com>
2020-04-28 08:51:05 +0900

    4.28 연습

---
+++ Jungol_기초다지기/src/배열1/형성평가01/Main.java
@@ -0,0 +1,19 @@
+package 배열1.형성평가01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String list[]= new String[10];
+		int i ;
+		int j ;
+		for(i=0; i<list.length; i++) {
+			list[i]=sc.next();
+		}
+		for(j=i-1; j>=0; j--) {
+		System.out.print(list[j]+" ");
+		}
+	}
+}//연습
---
+++ Jungol_기초다지기/src/배열1/형성평가02/Main.java
@@ -0,0 +1,26 @@
+package 배열1.형성평가02;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int list[]=new int[5];
+	int i;
+	int j;
+	int sum=0;
+	for(i=0; i<list.length; i++) {
+		list[i]=sc.nextInt();
+		}
+	for(j=0; j<i-1; j++)
+		if(j%2==0) {
+			sum = sum + list[j];
+	}else {
+		break;
+	}
+		
+	
+	}
+	
+	}
+
