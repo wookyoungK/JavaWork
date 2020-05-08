2c1a123215aa83b52a695e4720d18734e4673cdb
p11256 <pd1125@naver.com>
2020-05-08 10:13:00 +0900

    5.08-1

---
+++ Lec_WEB/WebContent/03_JavaScript/JS/myScript.js
@@ -0,0 +1,15 @@
+/**
+ * TODO
+ */
+
+ function light(sw){
+     var pic;
+     if(sw == 0){
+         pic= "https://www.w3schools.com/html/pic_bulboff.gif";
+
+     }else{
+         pic = "https://www.w3schools.com/html/pic_bulbon.gif";
+     }
+    //  <img>의 src값 변경
+     document.getElementById('myImage').src=pic;
+ }
