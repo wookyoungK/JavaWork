ab9003c64e373320c2d7573572013235fce23055
p11256 <pd1125@naver.com>
2020-05-11 15:29:08 +0900

    5.11-1

--- Programmers/src/com/lec/java/level2/Printer.java
+++ Programmers/src/com/lec/java/level2/Printer.java
@@ -3,8 +3,27 @@ package com.lec.java.level2;
 public class Printer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] priorities = { 1, 1, 9, 1, 2, 1 };
+		int location = 0;
+		int j=0;
 
+		int answer = 1;
+		while (true) {
+			for ( j = 0; j < priorities.length; j++) {
+				int maxnum = 0;
+				for (int i = 0; i < priorities.length; i++) {
+					if (priorities[i] > maxnum)
+						maxnum = priorities[i];
+				}
+				if (priorities[j] == maxnum) {
+					priorities[j] = 0;
+					if (j == location)
+						System.out.println(j);
+					else
+						j++;
+				}
+			}
+		
+		}
 	}
-
 }
