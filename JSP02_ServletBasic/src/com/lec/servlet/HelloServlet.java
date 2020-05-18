7d8b3a325d42ad7968e1b170767cd4bdf45db0c6
p11256 <pd1125@naver.com>
2020-05-18 17:55:38 +0900

    5.18_03

--- JSP02_ServletBasic/src/com/lec/servlet/HelloServlet.java
+++ JSP02_ServletBasic/src/com/lec/servlet/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.lec.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,9 +24,26 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 //		get방식 사용중 
-		
+
 		System.out.println("안녕하세요 첫번째 Servlet 입니다.");
-	}
+		//서블릿으로 HTML , 문서 response 하기
+		//1. content type 설정
+		//2. PrintWriter 객체 생성 <-- response 객체로부터
+		//3. PrintWriter 객체로 HTML 스트림에 쓰기.
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html lang='ko'>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>");
+		out.println("<title>서블릿 response</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>HTML문서 response</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
