cb68a11f4905deee7865dc5dda8c9b70c2b65f21
p11256 <pd1125@naver.com>
2020-06-09 10:33:26 +0900

    6.09

--- JSPEX_AJAX_SPA/WebContent/JS/board.js
+++ JSPEX_AJAX_SPA/WebContent/JS/board.js
@@ -50,7 +50,21 @@ function updateList(jsonObj){
 		$("#list tbody").html(result);  // 테이블 업데이트!
 		
 		// 페이지 정보 업데이트
-		$("#pageinfo").text(jsonObj.page + "/" + jsonObj.totalpage + "페이지, " + jsonObj.totalCnt + "개의 글");
+		$("#pageinfo").text(jsonObj.page + "/" + jsonObj.totalpage + "페이지, " + jsonObj.totalcnt + "개의 글");
+		
+		// pageRows
+		var txt = "<select id='rows'>\n";
+		txt += "<option" + ((window.pageRows == 10)?"selected":"") + ">10개씩</option>\n";
+		txt += "<option" + ((window.pageRows == 20)?"selected":"") + ">20개씩</option>\n";
+		txt += "<option" + ((window.pageRows == 50)?"selected":"") + ">50개씩</option>\n";
+		txt += "<option" + ((window.pageRows == 100)?"selected":"") + ">100개씩</option>\n";		
+		txt += "</select>\n";
+		$("#pageRows").html(txt);
+		
+		
+		// 페이징 업데이트
+		var pagination = buildPagination(jsonObj.writepages, jsonObj.totalpage, jsonObj.page, jsonObj.pagerows);
+		$("#pagination").html(pagination);
 		
 		return true;
 	} else {
@@ -58,7 +72,55 @@ function updateList(jsonObj){
 		return false;
 	}
 	return false;
-}
+} // end updateList()
+
+
+function buildPagination(writePages, totalPage, curPage, pageRows){
+	
+	var str = "";   // 최종적으로 페이징에 나타날 HTML 문자열 <li> 태그로 구성
+	
+	// 페이징에 보여질 숫자들 (시작숫자 start_page ~ 끝숫자 end_page)
+    var start_page = ( (parseInt( (curPage - 1 ) / writePages ) ) * writePages ) + 1;
+    var end_page = start_page + writePages - 1;
+
+    if (end_page >= totalPage){
+    	end_page = totalPage;
+    }
+    
+  //■ << 표시 여부
+	if(curPage > 1){
+		str += "<li><a onclick='loadPage(1)' class='tooltip-top' title='처음'><i class='fas fa-angle-double-left'></i></a></li>\n";
+	}
+	
+  	//■  < 표시 여부
+    if (start_page > 1) 
+    	str += "<li><a onclick='loadPage(" + (start_page - 1) + ")' class='tooltip-top' title='이전'><i class='fas fa-angle-left'></i></a></li>\n";
+    
+    //■  페이징 안의 '숫자' 표시	
+	if (totalPage > 1) {
+	    for (var k = start_page; k <= end_page; k++) {
+	        if (curPage != k)
+	            str += "<li><a onclick='loadPage(" + k + ")'>" + k + "</a></li>\n";
+	        else
+	            str += "<li><a class='active tooltip-top' title='현재페이지'>" + k + "</a></li>\n";
+	    }
+	}
+	
+	//■ > 표시
+    if (totalPage > end_page){
+    	str += "<li><a onclick='loadPage(" + (end_page + 1) + ")' class='tooltip-top' title='다음'><i class='fas fa-angle-right'></i></a></li>\n";
+    }
+
+	//■ >> 표시
+    if (curPage < totalPage) {
+        str += "<li><a onclick='loadPage(" + totalPage + ")' class='tooltip-top' title='맨끝'><i class='fas fa-angle-double-right'></i></a></li>\n";
+    }
+
+    return str;
+
+
+	
+} // end buildPagination()
 
 
 
