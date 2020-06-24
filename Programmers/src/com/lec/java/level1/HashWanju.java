ea0388f9dcd4f4ec091b9d082b5ec1d0624f60e5
pd11256 <pd1125@naver.com>
2020-06-24 23:22:09 +0900

    6.24
    programmers

--- Programmers/src/com/lec/java/level1/HashWanju.java
+++ Programmers/src/com/lec/java/level1/HashWanju.java
@@ -1,38 +1,38 @@
-//package com.lec.java.level1;
-//
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.Set;
-//
-//
-//public class HashWanju {
-//
-//	public static void main(String[] args) {
-//		String [] participant = {"leo", "kiki", "eden"};
-//		String [] completion = {"eden", "kiki"};
-//		String answer="";
-//		int a=0;
-//		
-//		HashMap<Integer, String> list =new HashMap<Integer, String>();
-//		
-//		for(int i=0; i<participant.length; i++) {
-//			list.put(participant.length,participant[i]);
-//		}
-//
-//		for(int i =0; i<participant.length; i++) {
-//			for(int j=0; j<completion.length; j++) {
-//				if(participant[i].equals(completion[j])) {
-//					list.remove(i+1);
-//				}
-//			}
-//		}
-//		Set<Integer> keySet =list.keySet();
-//		Iterator<String> itr = keySet.iterator();
-//		while(itr.hasNext()) {
-//			answer=itr.next();
-//		}
-//		
-//		System.out.println(answer);
-//	}
-//
-//}
+package com.lec.java.level1;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class HashWanju {
+
+	public static void main(String[] args) {
+		String [] participant = {"leo", "kiki", "eden"};
+		String [] completion = {"eden", "kiki"};
+		String answer="";
+		int a=0;
+		
+		HashMap<Integer, String> list =new HashMap<Integer, String>();
+		
+		for(int i=0; i<participant.length; i++) {
+			list.put(participant.length,participant[i]);
+		}
+
+		for(int i =0; i<participant.length; i++) {
+			for(int j=0; j<completion.length; j++) {
+				if(participant[i].equals(completion[j])) {
+					list.remove(i+1);
+				}
+			}
+		}
+		Set<Integer> keySet =list.keySet();
+		Iterator<String> itr = keySet.iterator();
+		while(itr.hasNext()) {
+			answer=itr.next();
+		}
+		
+		System.out.println(answer);
+	}
+
+}
