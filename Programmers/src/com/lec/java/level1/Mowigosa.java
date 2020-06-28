e5d79b9c8f2529d1414b8cdde29c620c52c6db5f
pd11256 <pd1125@naver.com>
2020-06-28 17:28:35 +0900

    6.28
    programmers

--- Programmers/src/com/lec/java/level1/Mowigosa.java
+++ Programmers/src/com/lec/java/level1/Mowigosa.java
@@ -1,16 +1,57 @@
 package com.lec.java.level1;
+
 //완전탐색 - 모의고사
 public class Mowigosa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		   int[] answer = {1,3,2,4,2};
+		   int [] answers= {};
+	        int[] a = {1, 2, 3, 4, 5};
+	        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+	        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+	        int[] cnt = {0,0,0};
+	        int max = 0;
+	        int maxcount=1;
+	        
+	        
+	        
+	        for (int i = 0; i < answer.length; i++) {
+	            if ( answer[i] == a[i%5]) {
+	                cnt[0]++;
+	            }
+	            if ( answer[i] == b[i%8]) {
+	                cnt[1]++;
+	            }
+	            if ( answer[i] == c[i%10]) {
+	                cnt[2]++;
+	            }
+	        }
+	        
+	        for(int i =0;i<cnt.length;i++){
+	        	if(cnt[i]==0) {
+	        		maxcount++;
+	        	}else if(max < cnt[i]) {
+	        		max=cnt[i];
+	        	}else {
+	        		maxcount++;
+	        	}
+	        	
+	        	answers[i]=maxcount;
+	        	
+	        	
+	        }
+	        
+//	            if(max < cnt[i]){
+//	                max = cnt[i];
+//	                maxcount = i+1;
+//	                answers = new int[maxcount];
+//	            }
+//	            if(max == cnt[i]){
+//	                maxcount++;
+//	            }
+//	        answer = new int[maxcount];
+//	        return answer;
+	    }
 	}
 
-}
-class Solution {
-    public int[] solution(int[] answers) {
-        int[] answer = {};
-        return answer;
-    }
-}
+
