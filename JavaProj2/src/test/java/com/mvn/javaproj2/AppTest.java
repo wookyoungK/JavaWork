d227eccc8e362d949ef0aa9fd97bbfc53d2e3695
pd11256 <pd1125@naver.com>
2020-07-06 14:20:53 +0900

    7.06
    Junit

---
+++ JavaProj2/src/test/java/com/mvn/javaproj2/AppTest.java
@@ -0,0 +1,44 @@
+package com.mvn.javaproj2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+//JUnit  으로 테스트 동작시킬 클래스와 메소드는 모두 public으로 만들자
+
+public class AppTest {
+ 
+	private App app = new App();
+
+	@Test
+	public void test1() {
+//		if ("welcome".equals(app.getWelcome())) {
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
+
+		//assertxxx() 메소드 사용
+		assertEquals("welcome",app.getWelcome());
+		assertEquals("Hello",app.getHello());
+		assertEquals("bye",app.getBye());
+		
+	}
+	
+	
+	@Test
+	public void test2() {
+		//assertxxx() 메소드 사용
+
+		assertEquals("Bye",app.getBye());
+		
+	}
+	@Test
+	public void test3() {
+		//assertxxx() 메소드 사용
+		
+		assertEquals("hello",app.getHello());
+		
+	}
+}
