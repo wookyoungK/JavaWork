4da47a73ac7775a219c0cc2142d7890fabc66f2a
pd11256 <pd1125@naver.com>
2020-08-09 22:57:13 +0900

    DB hoxy혹쉬

--- Hoxy/src/ERD/hoxy2.sql
+++ Hoxy/src/ERD/hoxy2.sql
@@ -1,48 +1,41 @@
-SELECT * FROM Hire;
-
-CREATE TABLE Hire
-(
-
-    h_uid number NOT NULL,
-    C_UID number not null,
-    h_name varchar2(200) NOT NULL,
-    h_title varchar2(200) NOT NULL,
-    h_content clob,
-    h_salary varchar2(50) NOT NULL,
-    h_position1 varchar2(100) NOT NULL,
-    h_position2 varchar2(100) NOT NULL,
-    h_part varchar2(10),
-    -- 중복되게...1011 이진수....
-    h_career varchar2(20) NOT NULL,
-    h_degree varchar2(20) NOT NULL,
-    h_workform varchar2(100) NOT NULL,
-    h_cnt number default 0 NOT NULL,
-    h_upDate varchar2(100) NOT NULL,
-    h_regDate varchar2(100) not null,
-    PRIMARY KEY (h_uid)
-);
-
+SELECT *FROM Hire;
+CREATE SEQUENCE hire_seq;
+Drop table Hire cascade constraints;
 DROP TABLE Hire CASCADE CONSTRAINTS;
 DROP SEQUENCE hire_seq;
 CREATE SEQUENCE hire_seq;
 CREATE SEQUENCE company_seq;
-DROP SEQUENCE company_seq;
+SELECT *FROM huser;
+
+CREATE TABLE Hire
+(
+
+    h_uid       number           NOT NULL,
+    c_uid      number           not null,
+    h_name      varchar2(400)    NOT NULL,
+    h_title     varchar2(400)    NOT NULL,
+    h_content   clob,
+    h_salary    varchar2(400)    NOT NULL,
+    h_position1 varchar2(200)    NOT NULL,
+    h_position2 varchar2(600)    NOT NULL,
+    h_part      varchar2(400),
+    -- 중복되게...1011 이진수....
+    h_career    varchar2(400)    NOT NULL,
+    h_degree    varchar2(400)    NOT NULL,
+    h_workform  varchar2(400)    NOT NULL,
+    h_cnt       number default 0 NOT NULL,
+    h_upDate    varchar2(400)    NOT NULL,
+    h_regDate   varchar2(400)    not null,
+    PRIMARY KEY (h_uid)
+);
 
 INSERT INTO Hire (h_uid,C_UID,h_name,h_title,h_content,h_salary,h_position1,h_position2,h_part,h_career,h_degree,h_workform,h_upDate,h_regDate)VALUES
-(hire_seq.nextval,1,'LG',
- '코리아it아카데미','첫번째','2000~3000','수원시','강남역','대리','사원','학력무관','인턴',sysdate,sysdate);
-INSERT INTO Hire (h_uid,C_UID,h_name,h_title,h_content,h_salary,h_position1,h_position2,h_part,h_career,h_degree,h_workform,h_upDate,h_regDate)VALUES
-(hire_seq.nextval,2,'SK','케이티뱅크','첫번째','2000~3000','서울시',
- '강남역','인턴','사원','초대졸','인턴',sysdate,sysdate);
-
+(hire_seq.nextval,1,'SAMSUNG',
+ '에스피테크놀리지','http://www.smilegate.com/ko/images/recruit/stoveImg/header_megaport.png','2400~2600','서울시','강남역','JAVA','경력','대졸','정규직','2020-08-04','2020-08-8');
 commit;
 
-select to_date(h_regDate, 'YYYY-MM-DD')-round(sysdate) from Hire;
 
 
-SELECT h_uid "uid",h_title title, h_career career,to_date(h_regDate, 'YYYY-MM-DD')-round(sysdate) remain
-FROM
-    HIRE
-ORDER
-    BY
-    h_uid DESC;
+
+
+
